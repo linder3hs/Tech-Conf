60880c937f375bbecbcd8359b7b8ce5b424ca1a4
linder3hs <linderhassinger00@gmail.com>
2023-02-08 01:54:51 -0300

    chore: home

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
 });
