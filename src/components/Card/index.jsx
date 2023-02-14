47954a2a4cf432f6d55a209eb42fab2fb108ca0e
linder3hs <linderhassinger00@gmail.com>
2023-02-13 23:52:53 -0500

    feat: tickets

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,104 @@
+import { useState, useEffect } from "react";
+import { getUser } from "../../services/auth";
+import {
+  insertDataIntoTable,
+  getTicketRecord,
+  getDataFromTable,
+} from "../../services/config";
+import Swal from "sweetalert2";
+import "./index.css";
+
+export default function Card(props) {
+  const { title, body, date, capacity, id } = props;
+
+  const [isSubscribed, setIsSubscribed] = useState(true);
+
+  const [tickets, setTickets] = useState(0);
+
+  const handleGetTickets = async () => {
+    const tickets = await getDataFromTable("tickets", {
+      key: "event_id",
+      value: id,
+    });
+
+    setTickets(tickets.length);
+  };
+
+  const handleIsSubscribed = async () => {
+    const { user } = await getUser();
+
+    const { length } = await getTicketRecord(id, user.id);
+
+    if (length) return;
+
+    setIsSubscribed(false);
+  };
+
+  const handleSubscribe = async () => {
+    const { user } = await getUser();
+
+    const response = await insertDataIntoTable("tickets", {
+      user_id: user.id,
+      event_id: id,
+    });
+
+    if (!response) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Algo salió mal, intentalo de nuevo",
+      });
+
+      return;
+    }
+
+    setIsSubscribed(true);
+    handleGetTickets();
+
+    Swal.fire({
+      icon: "success",
+      title: "¡Genial!",
+      text: "Te has inscrito al evento",
+    });
+
+    return;
+  };
+
+  useEffect(() => {
+    handleIsSubscribed();
+  }, []);
+
+  useEffect(() => {
+    handleGetTickets();
+  }, []);
+
+  return (
+    <div className="bg-white p-6 border border-gray-200 rounded-lg shadow">
+      <h2 className="font-bold text-2xl h-16">{title}</h2>
+      <p className="mt-5 h-20">{body}</p>
+
+      <p className="mt-10 text-blue-800 font-bold">{date}</p>
+      <div className="mt-10 flex items-center justify-between ">
+        {!isSubscribed ? (
+          <button
+            onClick={handleSubscribe}
+            className="bg-yellow-300 border-solid border-black border-2 p-2 font-bold rounded-full px-9"
+          >
+            Iscribirme
+          </button>
+        ) : (
+          <button
+            onClick={handleSubscribe}
+            className="bg-red-500 text-white p-2 font-bold rounded-full px-9"
+          >
+            Ver Ticket
+          </button>
+        )}
+
+        <p>
+          {tickets} / {capacity}
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/services/config.js
+++ src/services/config.js
@@ -8,11 +8,41 @@ const supabase = createClient(
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im52cGFrbmp4ZGdsbnJraGFobmFxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU5MzQ4MjMsImV4cCI6MTk5MTUxMDgyM30.yaixrMiLozraxdYQAKDkA5ta8H-fKn6NTcFAX25cXHU"
 );
 
-export async function getDataFromTable(table) {
+export async function getDataFromTable(table, condition = null) {
   const { data, error } = await supabase
     .from(table)
     .select("*")
     .order("id", { ascending: true });
+
+  if (error) {
+    Sentry.captureException(error);
+    return [];
+  }
+
+  if (condition) {
+    return data.filter((item) => item[condition.key] === condition.value);
+  }
+
+  return data;
+}
+
+export async function insertDataIntoTable(table, data) {
+  const { error } = await supabase.from(table).insert(data);
+
+  if (error) {
+    Sentry.captureException(error);
+    return false;
+  }
+
+  return true;
+}
+
+export async function getTicketRecord(event_id, user_id) {
+  const { data, error } = await supabase
+    .from("tickets")
+    .select("*")
+    .eq("event_id", event_id)
+    .eq("user_id", user_id);
 
   if (error) {
     Sentry.captureException(error);
