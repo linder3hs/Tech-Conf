bcf9a114c973c96595758f5ef5f04abb32b1fb5a
linder3hs <linderhassinger00@gmail.com>
2023-02-26 06:08:36 -0500

    chore: design

--- src/components/Sponsors/index.tsx
+++ src/components/Sponsors/index.tsx
@@ -16,9 +16,11 @@ export default function Sponsors() {
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <p className="md:text-6xl sm:text-lg text-black text-center md:w-3/5">
-        Un agradecimiento especial para nuestros Sponsors
-      </p>
+      <div className="md:w-3/5">
+        <h2 className="sm:mt-16 md:text-7xl lg:text-8xl font-extrabold text-center">
+          Nuestros Sponsors
+        </h2>
+      </div>
 
       <div className="grid lg:grid-cols-4 sm:grid-cols-1 mt-28 items-center gap-24">
         {sponsors.map((sponsor: ISponsor) => (
---
+++ src/interfaces/speaker.ts
@@ -0,0 +1,9 @@
+export interface ISpeaker {
+  id: number;
+  name: string;
+  image: string;
+  github: string;
+  about: string;
+  web: string;
+  created_at: string;
+}
