6ae92a0071a87c2e8254b3a6325569449d9d2f5f
linder3hs <linderhassinger00@gmail.com>
2023-05-08 13:50:56 -0500

    chore: schedule

--- src/components/Event/components/index.tsx
+++ src/components/Event/components/index.tsx
@@ -1,5 +1,5 @@
 export { default as Speakers } from "./Speakers";
 
-export { default as Sponsors } from "./sponsors";
+export { default as Sponsors } from "./Sponsors";
 
 export { default as Schedule } from "./Schedule";
