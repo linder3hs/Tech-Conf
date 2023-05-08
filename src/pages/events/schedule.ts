dcf1b6540d7846c9730027752ffb356e2a92e43c
linder3hs <linderhassinger00@gmail.com>
2023-05-08 13:48:28 -0500

    chore: schedule

--- src/pages/events/schedule.ts
+++ src/pages/events/schedule.ts
@@ -1,47 +1,58 @@
 export const schedule = [
   {
     title: "Hola mundo!, Bienvenida a la Tech Conf!",
-    time: "12:00",
+    time: "11:30 - 12:00",
     speaker: "Linder Hassinger - Victor Saico",
   },
   {
     title: "Aplicaciones Cloud en soluciones a medida - AWS | Azure",
-    time: "12:25",
+    time: "12:10 - 12:40",
     speaker: "Luis Cruz",
   },
   {
     title: "Almuerzo",
-    time: "13:05",
+    time: "12:40 - 13:40",
     speaker: "",
   },
   {
     title: "ITIL aplicado a las empresas Tech - Tecsup",
-    time: "14:10",
+    time: "13:40 - 14:10",
     speaker: "Jaime Gomez",
   },
   {
-    title: "El software aplicado al empredimiento",
-    time: "14:50",
-    speaker: "Bruno Díaz",
-  },
-  {
-    title: "La eduación a través de los bootcamp - CodiGO",
-    time: "15:30",
-    speaker: "Daniel de la Cruz",
+    title: "Emprendimiento en el mundo Tech - Startups",
+    time: "14:10 - 14:40",
+    speaker: "Bruno Diaz",
   },
   {
     title: "Marco de trabajado aplicado al código fuente (Git-Flow)",
-    time: "16:10",
+    time: "14:40 - 15:10",
     speaker: "Victor Saico",
   },
   {
+    title:
+      "Introducción a BTP y los servicios brindados en Cloud Foundry  y sus aplicaciones en el mundo laboral",
+    time: "15:10 - 15:40",
+    speaker: "Guillermo Narvaez",
+  },
+  {
+    title: "La eduación a través de los bootcamp - CodiGO",
+    time: "15:40 - 16:10",
+    speaker: "Daniel de la Cruz",
+  },
+  {
+    title: "Espacio de preguntas - Sorteos",
+    time: "16:10 - 16:40",
+    speaker: "",
+  },
+  {
     title: "Desarrolladores en tiempos de constante cambio",
-    time: "16:50",
+    time: "16:50 - 17:20",
     speaker: "Armando Picón",
   },
   {
     title: "Cierre",
-    time: "17:30",
+    time: "17:20 - 17:30",
     speaker: "Linder Hassinger - Victor Saico",
   },
 ];
